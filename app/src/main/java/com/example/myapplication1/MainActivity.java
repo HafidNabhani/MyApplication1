5c8456c25e831c4e2fa418da8d2faf0b4e2d3291
HafidNabhani <khairulnabhani@gmail.com>
2025-08-27 14:37:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int count=0;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.myTextView);
+
+    }
+
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        count++;
+        textView.setText(String.valueOf(count));
+    }
+}
